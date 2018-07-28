251679deb589bd558fc789426fb47cbd530507dc
Igor Mróz <mrozigor@wp.pl>
2018-07-28 14:13:30 +0200

    Fixed URL preparing.

--- src/utils/UrlManager.cpp
+++ src/utils/UrlManager.cpp
@@ -11,12 +11,14 @@ void im::utils::UrlManager::loadBaseURL()
     libconfig::Config config;
     config.readFile(".config");
     baseUrl = config.lookup("baseUrl").c_str();
+
     if (baseUrl.length() > 0 && baseUrl[baseUrl.length() - 1] != '/') {
         baseUrl += "/";
     }
-    if (config.exists("enableTLS") && config.lookup("enableTLS")) {
+
+    if (config.exists("enableTLS") && config.lookup("enableTLS") && (baseUrl.find("https://") == std::string::npos)) {
         baseUrl.insert(0, "https://");
-    } else {
+    } else if (baseUrl.find("http://") == std::string::npos) {
         baseUrl.insert(0, "http://");
     }
 }
