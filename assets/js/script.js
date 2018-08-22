d5c2fb3704374e22335f3bcb04fef2a010a0b0a2
Igor Mróz <mrozigor@wp.pl>
2018-08-22 18:31:16 +0200

    Refactored media queries. Removed unnecessary JS code. Added link to 'About me' page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,5 @@
-displayMenuCorrectly = function () {
-    if ($(window).width() < 400) { // TODO Refactor
-        $(".navigation__menu").addClass("navigation__menu--vertical");
-        $(".navigation__menu").hide();
-        $(".navigation__toggle-menu-button").show();
-    } else {
-        $(".navigation__menu").removeClass("navigation__menu--vertical");
-        $(".navigation__menu").show();
-        $(".navigation__toggle-menu-button").hide();
-    }
-}
-
-$(window).on('resize', displayMenuCorrectly);
-
 $(document).ready(function() {
-    displayMenuCorrectly();
-
     $(".navigation__toggle-menu-button").click(function() {
-        $(".navigation__menu").toggle();
+        $(".navigation__menu").toggleClass("navigation__menu--hide-vertical");
     });
 });
